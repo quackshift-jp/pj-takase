b7ca7c9e7aa78a15b81d43661968af2c838a24e2
apple <apple@appurunoMacBook-Pro.local>
2023-12-01 20:08:22 +0900

    add:GoogleVisionAPIメソッドの実装

--- src/main.py
+++ src/main.py
@@ -1,5 +1,9 @@
 from module import pdf_to_image
 from pathlib import Path
+from ocr import google_vision
 
 if __name__ == "__main__":
     pdf_to_image.convert_pdf_to_images(Path("./images"))
+    image = google_vision.read_image()
+    response = google_vision.extract_text(image)
+    print(response["fullTextAnnotation"]["text"].replace("\n", ""))
