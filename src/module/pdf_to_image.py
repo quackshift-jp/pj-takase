67b2da45ef0035b3c890b2de9a9276131715f391
apple <apple@appurunoMacBook-Pro.local>
2023-12-02 13:00:28 +0900

    add:モジュールの責務を分離して、モジュールはモジュール単体の責務だけをもつ

--- src/module/pdf_to_image.py
+++ src/module/pdf_to_image.py
@@ -4,8 +4,8 @@ from pdf2image import convert_from_path
 GOOGLE_DRIVE_PATH = Path("/Users/apple/Google Drive/マイドライブ/pj-takase")
 
 
-def convert_pdf_to_images(save_path: Path) -> None:
-    for pdf in GOOGLE_DRIVE_PATH.glob("*"):
+def convert_pdf_to_images(save_path: Path, search_keyword: str) -> None:
+    for pdf in GOOGLE_DRIVE_PATH.glob(f"*{search_keyword}*"):
         convert_from_path(
             pdf, output_folder=save_path, fmt="jpeg", output_file=pdf.stem, dpi=200
         )
