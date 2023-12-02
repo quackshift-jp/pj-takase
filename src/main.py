fa9b0d75a2599d12b40f7adba8f0ae7265e317f2
apple <apple@appurunoMacBook-Pro.local>
2023-12-02 13:01:08 +0900

    change:mainで処理内容の責務を持つようにする

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ from pathlib import Path
 from ocr import google_vision
 
 if __name__ == "__main__":
-    pdf_to_image.convert_pdf_to_images(Path("./images"))
-    image = google_vision.read_image()
+    pdf_to_image.convert_pdf_to_images(Path("./images"), search_keyword="重要事項説明書")
+    image = google_vision.read_image("images/重要事項説明書0001-1.jpg")
     response = google_vision.extract_text(image)
     print(response["fullTextAnnotation"]["text"].replace("\n", ""))
