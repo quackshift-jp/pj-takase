66d52c1ad5cdb0a1f6753d6e0334dc267c3c39fd
apple <apple@appurunoMacBook-Pro.local>
2023-12-06 13:00:31 +0900

    add:main関数で推論メソッドを実行する

--- src/main.py
+++ src/main.py
@@ -1,9 +1,15 @@
 from module import pdf_to_image
 from pathlib import Path
-from ocr import google_vision
+from ocr import google_vision, azure_computer_vision
 
 if __name__ == "__main__":
     pdf_to_image.convert_pdf_to_images(Path("./images"), search_keyword="重要事項説明書")
-    image = google_vision.read_image("images/重要事項説明書0001-1.jpg")
-    response = google_vision.extract_text(image)
-    print(response["fullTextAnnotation"]["text"].replace("\n", ""))
+    # image = google_vision.read_image("images/重要事項説明書0001-1.jpg")
+    # response = google_vision.extract_text(image)
+    # print(response["fullTextAnnotation"]["text"].replace("\n", ""))
+
+    operation_id = azure_computer_vision.get_operation_id(
+        "images/234119　重要事項説明書0001-1.jpg"
+    )
+    text = azure_computer_vision.extract_text(operation_id)
+    print(text)
